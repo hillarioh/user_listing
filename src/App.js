a26f6e70b0039db31ed6578cd77c416417099783
hillarioh <okerioh@gmail.com>
2022-03-24 15:01:49 +0300

    part 1 - present data on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { getStudents } from "utils/api";
+import Student from "components/student";
 
 function App() {
-  return <div className="App"></div>;
+  const [students, setStudents] = useState([]);
+
+  const getStudentList = async () => {
+    try {
+      let result = await getStudents();
+      setStudents(result?.data.students);
+    } catch (error) {
+      console.log(error?.response?.data);
+    }
+  };
+  useEffect(() => {
+    getStudentList();
+  }, []);
+
+  return (
+    <div className="App">
+      {students.map((stud) => (
+        <Student details={stud} key={stud.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
