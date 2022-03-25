74b67d198b77668c39c1efd22846a4a3239ed6c7
hillarioh <okerioh@gmail.com>
2022-03-25 05:14:39 +0300

    add search functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "app.scss";
 
 function App() {
   const [students, setStudents] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchList, setSearchList] = useState([]);
 
   const getStudentList = async () => {
     try {
@@ -14,6 +16,20 @@ function App() {
       console.log(error?.response?.data);
     }
   };
+
+  const handleSearch = () => {
+    let myRegex = new RegExp(`${search}`, "i");
+
+    const list = students?.filter((f) =>
+      myRegex.test(`${f.firstName} ${f.lastName}`)
+    );
+    setSearchList(list);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
   useEffect(() => {
     getStudentList();
   }, []);
@@ -21,9 +37,18 @@ function App() {
   return (
     <div className="main">
       <div className="students">
-        {students.map((stud) => (
-          <Student details={stud} key={stud.id} />
-        ))}
+        <div className="search-container">
+          <input
+            type="text"
+            name="fullname"
+            onChange={(e) => setSearch(e.target.value)}
+            className="search"
+            placeholder="Search by name"
+          />
+        </div>
+        {search
+          ? searchList.map((stud) => <Student details={stud} key={stud.id} />)
+          : students.map((stud) => <Student details={stud} key={stud.id} />)}
       </div>
     </div>
   );
