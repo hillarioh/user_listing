1e525f927a3bb3f211d5fbd7d4e4fdd48bd32913
hillarioh <okerioh@gmail.com>
2022-03-25 09:01:43 +0300

    part 5 - add tag with tag search functionality

---
+++ src/store/context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const studentsContext = createContext();
+
+export default studentsContext;
---
+++ src/store/reducer.js
@@ -0,0 +1,25 @@
+import * as actionTypes from "./types";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_STUDENTS:
+      return {
+        ...state,
+        students: [...state.students, ...action.payload.data],
+      };
+    case actionTypes.ADD_TAG:
+      const index = state.students.findIndex((x) => x.id === action.payload.id);
+      let student = state.students[index];
+      if (student.tags) {
+        student.tags = [...student.tags, action.payload.data];
+      } else {
+        student.tags = [action.payload.data];
+      }
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export { reducer };
